19ffaa54002e233283e8189de208835824a4dee0
F5 Education Services <99698691+learnf5@users.noreply.github.com>
2026-03-19 14:22:04 -0400

    Disable bigip1 config loading commands
    Commented out the commands for copying and loading the application template onto bigip1.

--- 20-CRTADM-L4-ADMIN.sh
+++ 20-CRTADM-L4-ADMIN.sh
@@ -1,8 +1,10 @@
 # set variable
 iApp=TS_iApp--station_v17.5.tmpl
 
-# download config from GitHub, copy to bigip1 and load/merge onto bigip1
+# download config from GitHub, 
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$iApp --output /home/student/Downloads/$iApp
-sudo scp /home/student/Downloads/$iApp 192.168.1.31:/shared/tmp
-sudo ssh 192.168.1.31 tmsh load /sys application template /shared/tmp/$iApp
-sudo ssh 192.168.1.31 tmsh save /sys config
+
+# [DISABLED] copy to bigip1 and load/merge onto bigip1
+# sudo scp /home/student/Downloads/$iApp 192.168.1.31:/shared/tmp
+# sudo ssh 192.168.1.31 tmsh load /sys application template /shared/tmp/$iApp
+# sudo ssh 192.168.1.31 tmsh save /sys config
